bad09cc534f72c77c78ce39b93255eb15c086e6c
GuillaumeCz <guillaume.couzy@gmail.com>
2018-06-11 18:36:15 +0200

    Ajout d'exemples avec MongoDb

--- server.js
+++ server.js
@@ -3,10 +3,30 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27016';
 
 MongoClient.connect(url, function(err, client) {
-	console.log(err, client);
 	if (err)
 		console.log('err', err);
-	else
-		console.log('ok');
 	var db = client.db('dpki');
+	insertDocuments(db, function() {
+		findDocuments(db, function() {
+
+			client.close();	
+		})
+	})
 })
+
+const insertDocuments = function(db, callback) {
+	const collection = db.collection('documents');
+	collection.insertMany([
+		{ a: 1 }, { b: 2 }, { c: 3 }
+	], function(err, result) {
+		callback(result);
+	})
+}
+
+const findDocuments = function(db, callback) {
+	const collection = db.collection('documents');
+	collection.find({'a': 1}).toArray(function(err, docs) {
+		console.log(docs)
+		callback(docs);
+	})
+}
