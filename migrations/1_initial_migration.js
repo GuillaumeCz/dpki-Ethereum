f6fd2092a32c193b8e462897c1d8ab60b3cb2cef
GuillaumeCz <guillaume.couzy@gmail.com>
2018-07-21 15:00:49 +0200

    Ajout des fichiers de migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
-var Dpki = artifacts.require("./Dpki.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-	deployer.deploy(Dpki);
+    deployer.deploy(Migrations);
 };
