f421f81d9c18709d465c2636df7fde79d82a40de
GuillaumeCz <guillaume.couzy@gmail.com>
2018-08-27 09:15:38 +0200

    fix migration bug

--- migrations/2_storage.js
+++ migrations/2_storage.js
@@ -2,7 +2,7 @@ var Dpki = artifacts.require("./Dpki.sol");
 var StoragePk = artifacts.require("StoragePk");
 var abs = artifacts.require("AbsDpki");
 
-modile.exports = function(deployer) {
+module.exports = function(deployer) {
     deployer.deploy(Dpki)
 		    .then(dpki => deployer.deploy(StoragePk, dpki.address));
 };
