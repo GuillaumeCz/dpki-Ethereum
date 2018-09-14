e643d70176dab5a920aaac74b4dfbde13a6e695e
GuillaumeCz <guillaume.couzy@gmail.com>
2018-09-14 23:55:59 +0200

    Update tests

--- cli/test/keys.test.js
+++ cli/test/keys.test.js
@@ -4,11 +4,9 @@ test('...should get the ipfs address of the key of Bob', () => {
     const name = 'Bob';
     const expectedAddress = 'QmWjkVAsR7fMnZYtueB2CevFA6GCHHLY3FdiSdCYAL85gT';
 
-    keys.keyAddrOf(name).then(res => {
-        console.log(res)
-        expect((res).toBe(expectedAddress));
-    }).catch(err => {
-        console.log(err)
-    })
-
+    keys.newKeyAddress(name, expectedAddress)
+        .then(() => keys.keyAddrOf(name))
+        .then(res => {
+            expect((res).toBe(expectedAddress));
+        });
 })
